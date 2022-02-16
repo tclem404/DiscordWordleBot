0f01195a24b5a1cc502dd4ca2c83bc53452161e1
tclem404 <72402233+tclem404@users.noreply.github.com>
2022-02-15 18:21:15 -0800

    Add files via upload

---
+++ Bot.py
@@ -0,0 +1,79 @@
+from random import randint
+import discord
+from discord.ext import commands
+
+from dotenv import load_dotenv
+
+import os
+import sys
+from os import getenv
+
+load_dotenv()
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.reply('Hello!')
+
+@bot.command()
+async def playWordle(ctx):
+    ansFile = open('ans.txt')
+    ansList = ansFile.read().split('\n')
+    answer = ansList[randint(0, len(ansList))]
+    
+    guessFile = open('guesses.txt')
+    guessList = guessFile.read().split('\n')
+
+    def check(m):
+        return m.channel == ctx.channel and m.author == ctx.author
+
+    guesses = 0
+    guessed = False
+
+    while guesses < 6 and not guessed:
+        # getting guess
+        await ctx.send('what is your guess?')
+        guesses += 1
+        msgFull = await bot.wait_for('message', check=check)
+        msg = msgFull.content
+
+        #cleaning guess
+        if(len(msg) != 5):
+            await ctx.send('Word Must Be 5 Letter Long')
+            guesses -= 1
+        elif not(msg in guessList):
+            await ctx.send('Word Not a Valid Guess')
+            guesses -= 1
+        else:
+            replyMsg = list('_____')
+            tempAns = list(answer)
+            for i in range(0,5):
+                if msg[i] == answer[i]:
+                    replyMsg[i] = 'G'
+                    tempAns[i] = '_'
+            for i in range(0,5):
+                if (replyMsg[i] == '_' and tempAns.__contains__(msg[i])):
+                    replyMsg[i] = 'Y'
+                    tempAns[tempAns.index(msg[i])] = '_'
+            
+            realReply = ''
+            for i in range(0,5):
+                if (replyMsg[i] == 'G'):
+                    realReply += ':green_square: '
+                elif (replyMsg[i] == 'Y'):
+                    realReply += ':yellow_square: '
+                else:
+                    realReply += ':black_large_square: '
+
+            await ctx.send(realReply)
+            if (replyMsg == list('GGGGG')):
+                guessed = True
+    
+    if (guessed):
+        await ctx.send('Congrats! In only ' + str(guesses) + ' guesses')
+    else:
+        await ctx.send('The word was ' + str(answer) + '. Better luck next time')
+            
+
+bot.run(getenv('TOKEN'))
